4d588045829610dbb733e416989dbf8ce5083f3c
alessdeam <alessandro.deamicis@student.univaq.it>
2022-02-22 00:40:17 +0100

    Delete

--- JustNote/src/main/java/mwt/justnote/JustNoteApplication.java
+++
@@ -1,9 +0,0 @@
-package mwt.justnote;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class JustNoteApplication extends Application {
-
-}
--- JustNote/src/main/java/mwt/justnote/model/AreaUtente.java
+++
@@ -1,8 +0,0 @@
-package mwt.justnote.model;
-
-public class AreaUtente {
-
-    private Utente utente;
-    private Area area;
-
-}
